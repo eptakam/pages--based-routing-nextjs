c362da74a3516d40a533886cf3de6fc0d70b7084
eptakam <emmataks@gmail.com>
2024-07-15 12:42:21 -0300

    A different way of setting Link hrefs.

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -12,7 +12,16 @@ export default function ClientsPage() {
       <ul>
         {clients.map((client) => (
           <li key={client.id}>
-            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+            {/* <Link href={`/clients/${client.id}`}>{client.name}</Link> */}
+            {/* NextJs donne egalement la possibilite de passer un objet a l'atribut href de la balise Link au lieu d'une chaine de caractere. voir ci-dessous */}
+            <Link
+              href={{
+                pathname: "/clients/[id]",
+                query: { id: client.id },
+              }}
+            >
+              {client.name}
+            </Link>
           </li>
         ))}
       </ul>
